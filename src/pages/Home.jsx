99c691e142db441caee63d3a6857aeee2b1200c5
vinay10082 <20bph043@nith.ac.in>
2022-11-07 22:35:36 +0530

    css better

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-
-import { render } from '@testing-library/react';
+import './Home.css'
 
 const Home = () => {
 
@@ -23,13 +22,13 @@ const Home = () => {
     <div className="main-container my-5 mx-5">
         <div className="user-container float-start">
         <>
-    <h2>List of Users</h2>
+    <h2 class="container p-3 my-3 bg-primary text-white rounded-right">List of Users</h2>
         {
           
           users.map((post) =>{
             const {id ,profile} = post;
             return (
-            <div className="card my-1" key={id}>
+            <div className="card my-5" key={id}>
               <p>({id}). {profile.firstName} <i class="btn btn-link text-decoration-none" onClick={() => setUserId(id-1)}>Show Info</i></p>
             </div>
           )}
@@ -38,24 +37,39 @@ const Home = () => {
     </>
         </div>
         <div className="userInfo-container float-end">
-            <h2>Selected User Info</h2>
+            <h2 class="container p-3 my-3 bg-dark text-white rounded-left">Selected User Info</h2>
             <>
             {
             userInfo == null ?
             <h1>Loading...</h1>
             :
               <>
-              <div className="card">
-              <img src={userInfo.avatar} alt="..." />
-              <h3>@ {userInfo.profile.username}</h3>
-              <p>{userInfo.Bio}</p>
-              Fullname:
-              <p>{userInfo.profile.firstName} {userInfo.profile.lastName}</p>
-              Job Title:
-              <p>{userInfo.jobTitle}</p>
-              email:
-              <p>{userInfo.profile.email}</p>
-            </div>
+            <div class="container d-flex justify-content-center mt-5">
+                 <div class="card p-5 py-5">
+                     <div class="text-center">
+                     <i class="fa fa-user-circle fa-5x" aria-hidden="true"></i>
+                         <p class="mt-1">@ {userInfo.profile.username}</p>
+                         <h3 class="mt-2">{userInfo.profile.firstName} {userInfo.profile.lastName}</h3>
+                         <span class="mt-1 clearfix">{userInfo.jobTitle}</span>
+                         <b>ABOUT</b><br></br>
+                         <small class="mt-4">{userInfo.Bio}</small>
+                         
+                         <div class="social-buttons mt-5"> 
+                             <button class="neo-button"><i class="fa fa-facebook fa-1x"></i> </button> 
+                             <button class="neo-button"><i class="fa fa-linkedin fa-1x"></i></button> 
+                             <button class="neo-button"><i class="fa fa-google fa-1x"></i> </button> 
+                             <button class="neo-button"><i class="fa fa-youtube fa-1x"></i> </button> 
+                             <button class="neo-button"><i class="fa fa-twitter fa-1x"></i> </button> 
+                         </div>
+                         
+                         <p class="mt-3"><b>Email</b><br></br>{userInfo.profile.email}</p>
+                     
+                     </div>
+                     
+                 
+                 </div>
+    
+             </div>
             </>
             }
             </>
